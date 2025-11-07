b27077cd2a687d4ed833a75bab10b06f78c7e0dc
ElvisMan2 <104602581+ElvisMan2@users.noreply.github.com>
2025-11-07 08:57:28 -0500

    project structure

---
+++ src/main/java/com/inetum/apigenerationloans/model/Client.java
@@ -0,0 +1,18 @@
+package com.inetum.apigenerationloans.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Client {
+
+    private Long clientId;
+
+    private String firstName;
+    private String paternalLastName;
+    private String maternalLastName;
+    private Double monthlyIncome;
+}
---
+++ src/test/java/com/inetum/apigenerationloans/ApiGenerationLoansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inetum.apigenerationloans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGenerationLoansApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
