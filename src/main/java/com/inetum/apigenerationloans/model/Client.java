c2a0485af3361bda93dbb6f1300fb634c8787d37
ElvisMan2 <104602581+ElvisMan2@users.noreply.github.com>
2025-11-07 19:18:28 -0500

    feat: JPA anotations

--- src/main/java/com/inetum/apigenerationloans/model/Client.java
+++ src/main/java/com/inetum/apigenerationloans/model/Client.java
@@ -1,18 +1,31 @@
-package com.inetum.apigenerationloans.model;
-
+package com.inetum.apisimulationloans.model;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "clients")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Client {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long clientId;
 
     private String firstName;
     private String paternalLastName;
     private String maternalLastName;
     private Double monthlyIncome;
+
+    @OneToMany(mappedBy ="client", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Simulation> simulations=new ArrayList<>();
+
+    @OneToMany(mappedBy ="client", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Loan> loans=new ArrayList<>();
 }
+
--- src/main/java/com/inetum/apigenerationloans/model/Loan.java
+++ src/main/java/com/inetum/apigenerationloans/model/Loan.java
@@ -1,17 +1,36 @@
-package com.inetum.apigenerationloans.model;
+package com.inetum.apisimulationloans.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "loans")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Loan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long loanId;
+
     private Double loanAmount;
     private Double interestRate;
     private Integer term; // months
     private Double installment;
     private Integer status; // 1: active, 0: inactive
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    @OneToOne(mappedBy = "loan")
+    private Simulation simulation;
+
+    @OneToMany(mappedBy ="loan", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<PaymentSchedule> paymentSchedule = new ArrayList<>();
 }
